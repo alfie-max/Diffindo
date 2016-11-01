d70e40b96cea8d297e400b47b72de91607b706c4
Clapinton <eric.olivetree@gmail.com>
2016-10-31 21:57:56 -0700

    Updating app name on config files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_WeCheck_session'
+Rails.application.config.session_store :cookie_store, key: '_Diffindo_session'
